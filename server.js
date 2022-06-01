d1b5d0fa8024580dc29f146c4e7f8ad062a7b19d
chedeline <vchedeline@gmail.com>
2022-06-01 09:51:34 -0400

    adds constant PORT

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => console.log("Listening..."));
+app.listen(PORT, () => console.log("Listening..."));
